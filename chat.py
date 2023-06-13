79f80f77bf11458bf25adc62d66e05110d1e00f5
Szymon <szymon.sadowski.m@gmail.com>
2023-06-13 14:15:51 +0200

    first commit after making prototype

---
+++ chat.py
@@ -0,0 +1,77 @@
+import socket
+import threading
+import os
+from encryption import Enc
+from receiving import Rcv
+import rsa
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Util.Padding import pad, unpad
+
+class Chat:
+    def __init__(self,rcv,enc):
+        self.rcv = rcv
+        self.enc = enc
+
+    def start_chat_await(self):
+        # (pub_key, priv_key) = rsa.newkeys(512)
+        # enc = Enc(pub_key, priv_key)
+        # rcv = Rcv(pub_key, priv_key)
+        #
+        listen_ip = '0.0.0.0'
+        listen_port = 888  # int(input('Enter listening port: '))
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((listen_ip, listen_port))
+
+        sock.listen(1)
+        print('Waiting for incoming connection...')
+
+        sending_sock, client_addr = sock.accept()
+        print('Connected to peer:', client_addr)
+
+        aes_key = get_random_bytes(16)
+        self.rcv.aes_key = aes_key
+
+        while True:
+            data = sending_sock.recv(1024).decode('utf-8')
+            if data.startswith('KEY'):
+                self.rcv.receive_public_key(data)
+                break
+
+        n, e = str(self.rcv.peer_key).split(",")
+        encrypted_key = self.enc.encrypt_message(aes_key, rsa.PublicKey(int(n), int(e)))
+        self.enc.send_aes_key(sending_sock, encrypted_key)
+
+        receive_thread = threading.Thread(target=self.rcv.receive_messages, args=(sending_sock,))
+        receive_thread.start()
+
+        #rcv.send_messages(sending_sock)
+
+    def start_chat_connect(self):
+        # (pub_key, priv_key) = rsa.newkeys(512)
+        # enc = Enc(pub_key, priv_key)
+        # rcv = Rcv(pub_key, priv_key)
+
+        target_ip = '192.168.0.18'  # input('Enter peer IP: ')
+        target_port = 888  # int(input('Enter peer port: '))
+
+        sending_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sending_sock.connect((target_ip, target_port))
+        print('Connected to peer')
+
+        self.enc.send_public_key(sending_sock)
+        while True:
+            data = sending_sock.recv(1024)
+            self.rcv.receive_aes_key(data)
+            break
+
+        receive_thread = threading.Thread(target=self.rcv.receive_messages, args=(sending_sock,))
+        receive_thread.start()
+
+
+
+
+        #rcv.send_messages(sending_sock, cipher)
+
+
