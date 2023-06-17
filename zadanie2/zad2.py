d2c5c9859287bb137f8817bff615cde6892be0e2
Łukasz <109036862+luka443@users.noreply.github.com>
2023-06-17 12:33:00 +0200

    Add files via upload

---
+++ zadanie2/zad2.py
@@ -0,0 +1,19 @@
+import rsa
+
+# wygeneruj klucze publiczne i prywatne
+# możesz posłużyć sie funkcją rsa.newkeys
+
+
+wiadmosc = "wiadmosc do zaszyfrowania"
+print("original ", wiadmosc)
+
+# Zaszyfruj wiadomość za pomocą algorytmu RSA i podanego klucza publicznego,
+#możesz posluzyc sie rsa.ecrypt oraz encode()
+
+
+#teraz ją wyprintuj
+print("zaszyfrowana wiadmosc : ",)
+
+# Roszyfruj wiadmosc
+
+print("odszyfrowana wiadmosc: ", )
